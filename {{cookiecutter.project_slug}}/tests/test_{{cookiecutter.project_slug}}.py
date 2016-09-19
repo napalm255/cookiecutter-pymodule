dd87db6424197684dc4b0fdae99d3baf7569e583
napalm255 <napalm255@gmail.com>
2016-09-18 22:27:26 -0400

    misc

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,41 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_{{ cookiecutter.project_slug }}
-----------------------------------
+"""Tests for `{{ cookiecutter.project_slug }}` module."""
 
-Tests for `{{ cookiecutter.project_slug }}` module.
-"""
-
-{% if cookiecutter.use_pytest == 'y' -%}
 import pytest
-{% else %}
-import sys
-import unittest
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
 from contextlib import contextmanager
 from click.testing import CliRunner
-{%- endif %}
-
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from {{ cookiecutter.project_slug }} import cli
-{%- endif %}
+from {{ cookiecutter.project_slug }}.__main__ import main
 
 
-{% if cookiecutter.use_pytest == 'y' -%}
 class Test{{ cookiecutter.project_slug|title }}(object):
 
     @classmethod
     def setup_class(cls):
         pass
 
-    def test_something(self):
-        pass
-
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
     def test_command_line_interface(self):
         runner = CliRunner()
         result = runner.invoke(cli.main)
@@ -45,33 +24,12 @@ class Test{{ cookiecutter.project_slug|title }}(object):
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
 
-{%- endif %}
+    def test_something(self):
+        pass
 
     @classmethod
     def teardown_class(cls):
         pass
-{% else %}
-class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_000_something(self):
-        pass
-{% if cookiecutter.command_line_interface|lower == 'click' %}
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
-
-{%- endif %}
 
 
 if __name__ == '__main__':
