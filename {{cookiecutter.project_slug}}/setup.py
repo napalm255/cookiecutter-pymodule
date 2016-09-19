f5a5913da4ecede2f69b314f281e66035ff4c240
napalm255 <napalm255@gmail.com>
2016-09-18 22:20:22 -0400

    changed syntax

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    {%- if cookiecutter.command_line_interface|lower == 'click' %}
+    {%- if 'click' in cookiecutter.command_line_interface|lower %}
     'Click>=6.0',
     {%- endif %}
     # TODO: put package requirements here
