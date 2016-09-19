43c4dc2dfe92dfeeb5440b8744b43d3fbb96dd66
napalm255 <napalm255@gmail.com>
2016-09-19 00:37:29 -0400

    clean up

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Test bake project."""
+
 from contextlib import contextmanager
 import shlex
 import os
 import sys
 import subprocess
-import yaml
 import datetime
+import yaml
 from cookiecutter.utils import rmtree
 
 from click.testing import CliRunner
@@ -17,8 +21,8 @@ else:
 
 @contextmanager
 def inside_dir(dirpath):
-    """
-    Execute code from inside the given directory
+    """Execute code from inside the given directory.
+
     :param dirpath: String, path of the directory the command is being run.
     """
     old_path = os.getcwd()
@@ -31,9 +35,10 @@ def inside_dir(dirpath):
 
 @contextmanager
 def bake_in_temp_dir(cookies, *args, **kwargs):
-    """
-    Delete the temporal directory that is created when executing the tests
-    :param cookies: pytest_cookies.Cookies, cookie to be baked and its temporal files will be removed
+    """Delete the temporal directory that is created when executing the tests.
+
+    :param cookies: pytest_cookies.Cookies, cookie to be baked and its
+                    temporal files will be removed
     """
     result = cookies.bake(*args, **kwargs)
     try:
@@ -43,8 +48,8 @@ def bake_in_temp_dir(cookies, *args, **kwargs):
 
 
 def run_inside_dir(command, dirpath):
-    """
-    Run a command from inside a given directory, returning the exit status
+    """Run a command from inside a given directory, returning the exit status.
+
     :param command: Command that will be executed
     :param dirpath: String, path of the directory the command is being run.
     """
@@ -53,27 +58,30 @@ def run_inside_dir(command, dirpath):
 
 
 def check_output_inside_dir(command, dirpath):
-    "Run a command from inside a given directory, returning the command output"
+    """Run a command from inside a given directory."""
     with inside_dir(dirpath):
         return subprocess.check_output(shlex.split(command))
 
 
-def test_year_compute_in_license_file(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        license_file_path = result.project.join('LICENSE')
-        now = datetime.datetime.now()
-        assert str(now.year) in license_file_path.read()
-
-
 def project_info(result):
-    """Get toplevel dir, project_slug, and project dir from baked cookies"""
+    """Get toplevel dir, project_slug, and project dir from baked cookies."""
     project_path = str(result.project)
     project_slug = os.path.split(project_path)[-1]
     project_dir = os.path.join(project_path, project_slug)
     return project_path, project_slug, project_dir
 
 
+def test_year_compute_in_license_file(cookies):
+    """Test year compute in license file."""
+    # pylint: disable=invalid-name
+    with bake_in_temp_dir(cookies) as result:
+        license_file_path = result.project.join('LICENSE')
+        now = datetime.datetime.now()
+        assert str(now.year) in license_file_path.read()
+
+
 def test_bake_with_defaults(cookies):
+    """Test bake with defaults."""
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
         assert result.exit_code == 0
@@ -88,45 +96,56 @@ def test_bake_with_defaults(cookies):
 
 
 def test_bake_and_run_tests(cookies):
+    """Test bake and run tests."""
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
-        run_inside_dir('python setup.py test', str(result.project)) == 0
+        assert run_inside_dir('python setup.py test', str(result.project)) == 0
         print("test_bake_and_run_tests path", str(result.project))
 
 
 def test_bake_withspecialchars_and_run_tests(cookies):
-    """Ensure that a `full_name` with double quotes does not break setup.py"""
-    with bake_in_temp_dir(cookies, extra_context={'full_name': 'name "quote" name'}) as result:
+    """Test bake with special characters and run tests.
+
+    Ensure that a `full_name` with double quotes does not break setup.py
+    """
+    # pylint: disable=invalid-name
+    context = {'full_name': 'name "quote" name'}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert result.project.isdir()
-        run_inside_dir('python setup.py test', str(result.project)) == 0
+        assert run_inside_dir('python setup.py test', str(result.project)) == 0
 
 
 def test_bake_and_run_travis_pypi_setup(cookies):
-    # given:
+    """Test bake and run travis pypi setup."""
+    # pylint: disable=invalid-name
     with bake_in_temp_dir(cookies) as result:
         project_path = str(result.project)
-
-        # when:
         travis_setup_cmd = ('python travis_pypi_setup.py'
-                            ' --repo napalm255/cookiecutter-pymodule --password invalidpass')
+                            ' --repo napalm255/cookiecutter-pymodule'
+                            ' --password invalidpass')
         run_inside_dir(travis_setup_cmd, project_path)
-        # then:
-        result_travis_config = yaml.load(result.project.join(".travis.yml").open())
+        travis_config = yaml.load(result.project.join(".travis.yml").open())
         min_size_of_encrypted_password = 50
-        assert len(result_travis_config["deploy"]["password"]["secure"]) > min_size_of_encrypted_password
+        password_len = len(travis_config["deploy"]["password"]["secure"])
+        assert password_len > min_size_of_encrypted_password
 
 
 def test_bake_without_travis_pypi_setup(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'use_pypi_deployment_with_travis': 'n'}) as result:
-        result_travis_config = yaml.load(result.project.join(".travis.yml").open())
-        assert "deploy" not in result_travis_config
-        assert "python" == result_travis_config["language"]
+    """Test bake without travis pypi setup."""
+    # pylint: disable=invalid-name
+    context = {'use_pypi_deployment_with_travis': 'n'}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
+        travis_config = yaml.load(result.project.join(".travis.yml").open())
+        assert "deploy" not in travis_config
+        assert "python" in travis_config["language"]
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'travis_pypi_setup.py' not in found_toplevel_files
 
 
 def test_bake_without_author_file(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'create_author_file': 'n'}) as result:
+    """Test bake without author file."""
+    context = {'create_author_file': 'n'}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'AUTHORS.rst' not in found_toplevel_files
         doc_files = [f.basename for f in result.project.join('docs').listdir()]
@@ -144,27 +163,35 @@ def test_bake_without_author_file(cookies):
 
 
 def test_make_help(cookies):
+    """Test make help."""
     with bake_in_temp_dir(cookies) as result:
-        output = check_output_inside_dir('make help', str(result.project))
+        output = str(check_output_inside_dir('make help', str(result.project)))
         assert b"check code coverage quickly with the default Python" in output
 
 
 def test_bake_selecting_license(cookies):
+    """Test bake selecting license."""
+    # pylint: disable=line-too-long
     license_strings = {
         'MIT license': 'MIT ',
-        'BSD license': 'Redistributions of source code must retain the above copyright notice, this',
+        'BSD license': 'Redistributions of source code'
+                       ' must retain the above copyright notice, this',
         'ISC license': 'ISC License',
-        'Apache Software License 2.0': 'Licensed under the Apache License, Version 2.0',
+        'Apache Software License 2.0': 'Licensed under the Apache License,'
+                                       ' Version 2.0',
         'GNU General Public License v3': 'GNU GENERAL PUBLIC LICENSE',
     }
-    for license, target_string in license_strings.items():
-        with bake_in_temp_dir(cookies, extra_context={'open_source_license': license}) as result:
+    for license_string, target_string in license_strings.items():
+        context = {'open_source_license': license_string}
+        with bake_in_temp_dir(cookies, extra_context=context) as result:
             assert target_string in result.project.join('LICENSE').read()
-            assert license in result.project.join('setup.py').read()
+            assert license_string in result.project.join('setup.py').read()
 
 
 def test_bake_not_open_source(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'open_source_license': 'Not open source'}) as result:
+    """Test bake not open source."""
+    context = {'open_source_license': 'Not open source'}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'setup.py' in found_toplevel_files
         assert 'LICENSE' not in found_toplevel_files
@@ -172,35 +199,42 @@ def test_bake_not_open_source(cookies):
 
 
 def test_using_pytest(cookies):
+    """Test using pytest."""
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
-        test_file_path = result.project.join('tests/test_python_boilerplate.py')
-        lines = test_file_path.readlines()
+        test_file = result.project.join('tests/test_python_boilerplate.py')
+        lines = test_file.readlines()
         assert "import pytest" in ''.join(lines)
 
 
 def test_project_with_invalid_module_name(cookies):
-    result = cookies.bake(extra_context={'project_name': 'something-with-a-dash'})
+    """Test project with invalid module name."""
+    # pylint: disable=invalid-name
+    result = cookies.bake(extra_context={'project_name': 'with-a-dash'})
     assert result.project is None
     result = cookies.bake()
     project_path = str(result.project)
 
     # when:
     travis_setup_cmd = ('python travis_pypi_setup.py'
-                        ' --repo napalm255/cookiecutter-pymodule --password invalidpass')
+                        ' --repo napalm255/cookiecutter-pymodule'
+                        ' --password invalidpass')
     run_inside_dir(travis_setup_cmd, project_path)
 
     # then:
-    result_travis_config = yaml.load(open(os.path.join(project_path, ".travis.yml")))
+    result_travis_config = yaml.load(open(os.path.join(project_path,
+                                                       ".travis.yml")))
     assert "secure" in result_travis_config["deploy"]["password"],\
         "missing password config in .travis.yml"
 
 
 def test_bake_with_console_script_files(cookies):
+    """Test bake with console script files."""
+    # pylint: disable=unused-variable, invalid-name
     result = cookies.bake()
     project_path, project_slug, project_dir = project_info(result)
     found_project_files = os.listdir(project_dir)
-    assert "cli.py" in found_project_files
+    assert "__main__.py" in found_project_files
 
     setup_path = os.path.join(project_path, 'setup.py')
     with open(setup_path, 'r') as setup_file:
@@ -208,9 +242,11 @@ def test_bake_with_console_script_files(cookies):
 
 
 def test_bake_with_console_script_cli(cookies):
+    """Test bake with console script cli."""
+    # pylint: disable=unused-variable, invalid-name
     result = cookies.bake()
     project_path, project_slug, project_dir = project_info(result)
-    module_path = os.path.join(project_dir, 'cli.py')
+    module_path = os.path.join(project_dir, 'python_boilerplate.py')
     module_name = '.'.join([project_slug, 'cli'])
     if sys.version_info >= (3, 5):
         spec = importlib.util.spec_from_file_location(module_name, module_path)
@@ -222,10 +258,11 @@ def test_bake_with_console_script_cli(cookies):
     else:
         cli = imp.load_source(module_name, module_path)
     runner = CliRunner()
-    noarg_result = runner.invoke(cli.main)
+    noarg_result = runner.invoke(cli.cli)
     assert noarg_result.exit_code == 0
-    noarg_output = ' '.join(['Replace this message by putting your code into', project_slug])
+    noarg_output = ' '.join(['Replace this message by putting your code into',
+                             project_slug])
     assert noarg_output in noarg_result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
     assert 'Show this message' in help_result.output
