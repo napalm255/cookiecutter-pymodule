3d40c7d0c5735194a9b2be2cd39dea333104ca6f
napalm255 <napalm255@gmail.com>
2016-09-18 22:31:42 -0400

    misc updates

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -6,7 +6,7 @@
 import pytest
 from contextlib import contextmanager
 from click.testing import CliRunner
-from {{ cookiecutter.project_slug }}.__main__ import main
+from {{ cookiecutter.project_slug }}.__main__ import cli
 
 
 class Test{{ cookiecutter.project_slug|title }}(object):
