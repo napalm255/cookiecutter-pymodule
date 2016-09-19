ddb7d607c3a0c29992b59ba4e77f0514f7a78d5e
napalm255 <napalm255@gmail.com>
2016-09-18 22:45:13 -0400

    misc updates

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -2,16 +2,13 @@
 # -*- coding: utf-8 -*-
 """Main entry point."""
 
-import click
+import {{cookiecutter.project_slug}}
 
-@click.command()
-def cli(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
-    click.echo('Replace this message by putting your code into '
-               '{{cookiecutter.project_slug}}.cli.main')
-    click.echo('See click documentation at http://click.pocoo.org/')
-    click.echo('Arguments: ', args)
+
+def main():
+    """Main entry point."""
+    {{cookiecutter.project_slug}}.cli()
 
 
 if __name__ == "__main__":
-    cli()
+    main()
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Main entry point."""
+
+import click
+
+@click.command()
+def cli(args=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo('Replace this message by putting your code into '
+               '{{cookiecutter.project_slug}}.cli.main')
+    click.echo('See click documentation at http://click.pocoo.org/')
+    click.echo('Arguments: ', args)
+
+
+if __name__ == "__main__":
+    cli()
