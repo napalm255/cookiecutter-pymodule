45f7cbde605fb74e6e84b6c46dfb1b41441fbfb7
napalm255 <napalm255@gmail.com>
2016-09-18 21:48:59 -0400

    clean up

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -9,7 +9,9 @@ MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 MODULE_NAME = '{{ cookiecutter.project_slug }}'
 
 if not re.match(MODULE_REGEX, MODULE_NAME):
-    print('ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % MODULE_NAME)
+    print('ERROR: The project slug (%s)' % MODULE_NAME,
+          'is not a valid Python module name.',
+          'Please do not use a - and use _ instead')
 
     # exit to cancel project
     sys.exit(1)
