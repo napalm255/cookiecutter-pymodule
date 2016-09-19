fbe2b39fb29a58ab1d5cd3f19124049d54f12422
napalm255 <napalm255@gmail.com>
2016-09-18 22:06:59 -0400

    clean up

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Setup module."""
 
 from setuptools import setup
 
@@ -44,7 +45,7 @@ setup(
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
     entry_points={
         'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main'
+            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.__main__:main'
         ]
     },
     {%- endif %}
